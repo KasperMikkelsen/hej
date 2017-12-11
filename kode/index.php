2506459e49890e6e1cc53f6f161e3d1311bfe491
Andreas <kanzz@live.dk>
2017-12-11 09:28:52 +0100

    Added labels and connection to database

--- kode/index.php
+++ kode/index.php
@@ -1,17 +1,27 @@
-<?php 
-<!DOCTYPE html>
+<?php
+
+	include 'connectDB';
+
+?>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
 	<form>
+		<label>Username:</label>
 		<input type="text" name="username">
+		<br>
+		<label>Password:</label>
 		<input type="password" name="password">
+		<br>
+		<label>Your full name:</label>
 		<input type="text" name="navn">
+		<br>
+		<label>Your age:</label>
 		<input type="text" name="alder">
+		<br>
 		<input type="submit" name="create" value="Create">
 	</form>
 </body>
 </html>
-?>
