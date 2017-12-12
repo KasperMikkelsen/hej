053b994354b51b735b508424b87d3d79036dbf0d
KasperMikkelsen <mikkelsenkasper75@gmail.com>
2017-12-12 08:54:33 +0100

    added a test page and a link to it from index.php

--- kode/index.php
+++ kode/index.php
@@ -10,6 +10,7 @@
   <div class="dropdown-content">
     <a href="createUser.php">Signup</a>
     <a href="login.php">Log-in</a>
+    <a href="testPage.php">test page</a>
     <!-- <a href="#">link</a>
     <a href="#">link</a> -->
   </div>
---
+++ kode/testPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>test page</title>
+</head>
+<header>
+	
+</header>
+
+<body>
+
+</body>
+</html>
