6710e2e9ae03c52464ecbacb065a2d94f2595eff
KasperMikkelsen <mikkelsenkasper75@gmail.com>
2017-12-13 08:19:57 +0100

    removed path to test site

--- kode/index.php
+++ kode/index.php
@@ -13,7 +13,7 @@
   <div class="dropdown-content">
     <a href="createUser.php">Signup</a>
     <a href="login.php">Log-in</a>
-    <a href="testPage.php">test page (status: open)</a>
+   
     <!-- <a href="#">link</a>
     <a href="#">link</a> -->
   </div>
